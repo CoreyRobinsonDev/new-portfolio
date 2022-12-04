5cdfc2ed58df902eb5bf5e34ad09c24e565b6e17
CoreyRobinsonDev <coreyrob52@gmail.com>
2022-12-04 13:58:31 -0500

    styled both home page and navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,14 @@
 import Link from "next/link";
+import styles from "../styles/Navbar.module.css";
+import FX from "../styles/FX.module.css";
 
 const Navbar = () => {
-  return <nav>
-    <ul>
-      <li><Link href="/">Home</Link></li>
-      <li><Link href="/">Projects</Link></li>
-      <li><Link href="/">Skills</Link></li>
+  return <nav className={styles.container}>
+    <ul className={styles.list}>
+      <li className={styles.list_item}><Link href="/" className={FX["double-hover"]}>Home</Link></li>
+      <li className={styles.list_item}><Link href="/" className={FX["double-hover"]}>Projects</Link></li>
+      <li className={styles.list_item}><Link href="/" className={FX["double-hover"]}>Skills</Link></li>
+      <li className={styles.list_item}><Link href="#contact" className={FX["double-hover"]}>Contact</Link></li>
     </ul>
   </nav>
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,10 +10,12 @@ export default function Home() {
       <title>Corey Robinson | Home</title>
     </Head>
     <section className={styles.container}>
-      <h2 className={styles.title}>FRONT-END ENGINEER</h2>
-      <h1 className={styles.name}>COREY ROBINSON</h1>
-      <p className={styles.description}>{"I solve business problems with front-end solutions. I find passion in building responsive, accessible, and search-engine-optimized web apps, always looking ahead and ready to learn new things."}</p>
-      <Link href="/projects" className={`${styles.link} ${FX.hover}`}><span className={styles.link__circle}><BsArrowRightShort /></span>Jump To Projects</Link>
+      <div className={styles.backdrop}>
+        <h2 className={styles.title}>FRONT-END ENGINEER</h2>
+        <h1 className={styles.name}>COREY ROBINSON</h1>
+        <p className={styles.description}>{"I solve business problems with front-end solutions. I find passion in building responsive, accessible, and search-engine-optimized web apps, always looking ahead and ready to learn new things."}</p>
+        <Link href="/projects" className={`${styles.link} ${FX.hover}`}><span className={styles.link__circle}><BsArrowRightShort /></span>Jump To Projects</Link>
+      </div>
     </section>
   </>
 }
