ac52787ec93daf62d215ba4b2311989484a47a27
CoreyRobinsonDev <coreyrob52@gmail.com>
2022-12-08 05:38:55 -0500

    improve animation design and responsiveness

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,11 +6,6 @@ import styles from "../styles/Navbar.module.css";
 import FX from "../styles/FX.module.css";
 
 const Navbar = () => {
-  const addClass = (e: any) => {
-    e.target.parentNode.parentNode.childNodes.forEach((child: any) => child.children[0].classList.remove(styles.focus))
-    e.target.classList.add(styles.focus);
-  }
-
   const { scrollY } = useScroll()
   const [isCollapsed, setIsCollapsed] = useState(false);
   const [y, setY] = useState(0);
@@ -30,16 +25,16 @@ const Navbar = () => {
     ? { y: -70, opacity: 1 }
     : { y: -130, opacity: 0 };
   
-  return <nav className={`${styles.container}`}>
-    <motion.ul className={styles.list} animate={{color: y >= 100 ? "black" : "white"}} onMouseEnter={() => setIsCollapsed(false)} onMouseLeave={() => y >= 100 ? setIsCollapsed(true) : null}>
-      <motion.li className={styles.list_item} animate={animate}><Link href="/" className={FX["double-hover"]} onClick={(e) => addClass(e)}>Home</Link></motion.li>
-      <motion.li className={styles.list_item} animate={animate}><Link href="/projects" className={FX["double-hover"]} onClick={(e) => addClass(e)}>Projects</Link></motion.li>
-      <motion.li className={styles.list_item} animate={animate}><Link href="/skills" className={FX["double-hover"]} onClick={(e) => addClass(e)}>Skills</Link></motion.li>
-      <motion.li className={styles.list_item} animate={animate}><Link href="#contact" className={FX["double-hover"]} onClick={(e) => addClass(e)}>Contact</Link></motion.li>
+  return <motion.nav className={`${styles.container}`} onMouseEnter={() => setIsCollapsed(false)} onMouseLeave={() => y >= 100 ? setIsCollapsed(true) : null}>
+    <motion.ul className={styles.list} animate={{color: y >= 100 ? "black" : "white"}}>
+      <motion.li className={styles.list_item} animate={animate}><Link href="/" className={FX["double-hover"]}>Home</Link></motion.li>
+      <motion.li className={styles.list_item} animate={animate}><Link href="/projects" className={FX["double-hover"]}>Projects</Link></motion.li>
+      <motion.li className={styles.list_item} animate={animate}><Link href="/skills" className={FX["double-hover"]}>Skills</Link></motion.li>
+      <motion.li className={styles.list_item} animate={animate}><Link href="#contact" className={FX["double-hover"]}>Contact</Link></motion.li>
     </motion.ul>
     <motion.hr className={styles.bar} animate={bars}></motion.hr>
     <motion.hr className={styles.bar} animate={bars}></motion.hr>
-  </nav>
+  </motion.nav>
 }
 
 export default Navbar;
